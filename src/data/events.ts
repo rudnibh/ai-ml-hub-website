e3d8c9c50ee0215cf7490b97272216130a1aadfc
Aarnya Jain <aarnyajain417@gmail.com>
2025-09-13 22:07:05 +0530

    Updated the events section

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,20 +1,20 @@
 import type { Event } from '../types/event';
 
 export const events: Event[] = [
-  {
-    title: 'Orientation of the AI/ML Hub | Join Us to deep dive in the world of AI and Machine Learning',
-    date: '27th August 2025 | 5:00 PM | LT-3 (ABB 1)',
-    time: '5:00 PM - 7:00 PM',
-    location: 'LT-3 (ABB 1)',
-    description: 'Are you passionate about technology? Curious about the world of Artificial Intelligence and Machine Learning? Eager to be part of a vibrant, innovative, and future-driven community? If yes — this is your chance!',
-    imageUrl: 'roboto.png',
-    registrationLink: 'https://docs.google.com/forms/d/e/1FAIpQLScmIQ5uEz5muSAqcVjdnYzYxGMYu8KCAvZiKkrgzWwoZuIwjg/viewform',
-    whatsappLink: 'https://chat.whatsapp.com/FRsxniPp9dj0MN312alqXH',
-    //certificate: 'Certificates provided!',
-    status: 'upcoming',
-    isFeatured: true,
-    galleryUrl: " ",
-  },
+  // {
+  //   title: 'Orientation of the AI/ML Hub | Join Us to deep dive in the world of AI and Machine Learning',
+  //   date: '27th August 2025 | 5:00 PM | LT-3 (ABB 1)',
+  //   time: '5:00 PM - 7:00 PM',
+  //   location: 'LT-3 (ABB 1)',
+  //   description: 'Are you passionate about technology? Curious about the world of Artificial Intelligence and Machine Learning? Eager to be part of a vibrant, innovative, and future-driven community? If yes — this is your chance!',
+  //   imageUrl: 'roboto.png',
+  //   registrationLink: 'https://docs.google.com/forms/d/e/1FAIpQLScmIQ5uEz5muSAqcVjdnYzYxGMYu8KCAvZiKkrgzWwoZuIwjg/viewform',
+  //   whatsappLink: 'https://chat.whatsapp.com/FRsxniPp9dj0MN312alqXH',
+  //   //certificate: 'Certificates provided!',
+  //   status: 'upcoming',
+  //   isFeatured: true,
+  //   galleryUrl: " ",
+  // },
   {
     title: "GENESIS : Introduction to Gen-AI",
     date: "April 2nd-5th, 2025",
